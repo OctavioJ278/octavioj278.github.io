d97708695412b70f809faaaff7843011cfaac7a8
OctavioJ278 <123426639+OctavioJ278@users.noreply.github.com>
2023-01-23 21:28:55 -0300

    Add files via upload

---
+++ funcion.js
@@ -0,0 +1,4 @@
+document.getElementById('boton').onclick = function () {
+    console.log("capturamos el evento click")
+    document.getElementById("boton").innerHTML = "Fecha de nacimiento: 14/06/1977 Genero: Masculino"
+}
